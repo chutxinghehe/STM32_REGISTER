a26cb29e94df801f3226e9e54c347d364510e1a0
chutxinghehe <xuantuan562005@gmail.com>
2026-09-03 04:17:18 +0700

    hihidodo

--- _ADC.c
+++ _ADC.c
@@ -6,7 +6,7 @@
 //____________________________________________________________________________________________________________________________
 //					ADC TO READ POTENTIAL (USE FOR 72MHZ)
 //----------------------------------------------------------------------------------------------------------------------------
-/*
+/** 1 channel adc -> PA5
 	------------------------------------------------------------
 	- this functinois use to read the potential
 	- gpio use in this case is Ch5 which mean use the pin A5
@@ -86,16 +86,105 @@ uint16_t value_ADC2_CH5()
 
 
 
+//____________________________________________________________________________________________________________________________
+//					ADC TO READ joystick (USE FOR 72MHZ)
+//----------------------------------------------------------------------------------------------------------------------------
+/** @idk  use 2 channel; PA5 -> VRx          PA7 ->VRy       && use DMA1
+
+	- don't use interrup
+	- use adc1 (channel 5 and 7); dma1
+	- use dma; circular buffer; continous; ....
+	
+	@note DMA will auto clear 'EOC bit'   so -> ignore to clear this bit
+	@note Pin '5v' of joystick need to wire to pin '3v3' in stm32
+*/
+static volatile uint16_t mSample[2] = {0, 0};
 
 
+void Init_readJoystick()
+{
+	//---------------------------------------
+	RCC_enable_PERIPH(mADC1);
+	RCC->AHBENR |= RCC_AHBENR_DMA1EN ;
+	
+	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV8 ;		// 72Mhz / 8 = 9Mhz for ADC
+	RCC->AHBENR |= RCC_AHBENR_DMA1EN ;			// DMA1
+	
+	
+	GPIO_Type joy_x;
+	joy_x.port	= GPIOA;
+	joy_x.pin		= 5;
+	joy_x.mode	= INPUT_MODE;
+	joy_x.mode_type 	= INPUT_ANALOG;
+	gpio_init(joy_x);
+	
+	
+	GPIO_Type joy_y;
+	joy_y.port	= GPIOA;
+	joy_y.pin	= 7;
+	joy_y.mode	= INPUT_MODE;
+	joy_y.mode_type 	= INPUT_ANALOG;
+	gpio_init(joy_y);
+	
+	//_____________________________ CONFIG ADC AND DMA __________________________________________
+	
+	// channel and sample time
+	ADC1->SMPR2 |= ADC_SMPR2_SMP5_2 | ADC_SMPR2_SMP5_1 | ADC_SMPR2_SMP5_0 ;	
+	ADC1->SMPR2 |= ADC_SMPR2_SMP7_2 | ADC_SMPR2_SMP7_1 | ADC_SMPR2_SMP7_0 ;	
+	
+	
+	// clear bit in length to pick 2 conversion
+	ADC1->SQR1 &= ~(ADC_SQR1_L) ;
+	ADC1->SQR1 |= (ADC_SQR1_L_0) ;
+	
+	
+	// init the firt conversion &  2rd
+	ADC1->SQR3 |= (ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_2) ;						// write channel 5 in 1st conversion
+	ADC1->SQR3 |= (ADC_SQR3_SQ2_0 | ADC_SQR3_SQ2_1 | ADC_SQR3_SQ2_2) ;		// WRITE_REG channel 7 in 2st conversion
+	
+	
+	// scan mode and enable dma
+	ADC1->CR1 |= ADC_CR1_SCAN ;
+	ADC1->CR2 |= ADC_CR2_DMA ;
+	
+	
+	// CONFIG DMA
+	DMA1_Channel1->CPAR = ((uint32_t) (&(ADC1->DR)) );		// address of periph
+	DMA1_Channel1->CMAR = (uint32_t) mSample ;			// addr of memory
+	DMA1_Channel1->CNDTR = 2; 				//number of the data transfer
+	DMA1_Channel1->CCR |= DMA_CCR1_PSIZE_0 	;	
+	DMA1_Channel1->CCR |= DMA_CCR1_MSIZE_0 ;
+	DMA1_Channel1->CCR |= DMA_CCR1_MINC | DMA_CCR1_CIRC | DMA_CCR1_EN ;		// Memory increase mode ; circular ; enable
+	
+	
+	ADC1->CR2 |= ADC_CR2_CONT ;			// continous conversion
+	ADC1->CR2 |= ADC_CR2_ADON ;			// wake up the adc
+	
+	delay_us(1);
+	
+	
+	ADC1->CR2 |= ADC_CR2_RSTCAL;	// reset calib
+    while (ADC1->CR2 & ADC_CR2_RSTCAL);
+	
+	ADC1->CR2 |= ADC_CR2_CAL ;		//calibr  
+	while (ADC1->CR2 & ADC_CR2_CAL);
+	
+	ADC1->CR2 |= ADC_CR2_ADON ;			// again -> start
+	delay_us(1);
+}
 
 
+uint16_t readVRX()
+{
+	return mSample[0]; // Channel 5 (Rank 1)
+}
 
+uint16_t readVRY()
+{
+	return mSample[1]; // Channel 7 (Rank 2)
+}
 
-
-
-
-
+//
 
 
 
--- main.c
+++ main.c
@@ -6,19 +6,14 @@
 #include "_ADC.h"
 
 
-volatile uint32_t check;
-
-
-
-
 int main(void)
 {
 	initSystick();
-	
 	init_TIM2();
 	
 	
-	//---------------------------------------
+	
+	//_______________	INIT GPIO	___________________________________________________________
 	GPIO_Type PC13;
 	PC13.port	= GPIOC;
 	PC13.pin	= 13;
@@ -28,64 +23,16 @@ int main(void)
 	gpio_init(PC13);
 	
 	
-
-
-	
-	//---------------------------------------
-	RCC_enable_PERIPH(mADC2);
-	RCC->CFGR |= RCC_CFGR_ADCPRE_DIV8 ;		// 72Mhz / 8 = 9Mhz for ADC
-	
-	GPIO_Type pot;
-	pot.port	= GPIOA;
-	pot.pin		= 5;
-	pot.mode	= INPUT_MODE;
-	pot.mode_type 	= INPUT_ANALOG;
-	gpio_init(pot);
-	
-	
-	
-	ADC2->CR1 |= ADC_CR1_EOCIE ;	// interrupr generated
-	NVIC_EnableIRQ(ADC1_2_IRQn) ;	// en nvic interrupt
-	
-	ADC2->SMPR2 |= ADC_SMPR2_SMP5_2 | ADC_SMPR2_SMP5_1 | ADC_SMPR2_SMP5_0 ;		// channel and sample time
-	ADC2->SQR1 &= ~(ADC_SQR1_L) ;		// clear bit in length to pick 1 conversion
-	ADC2->SQR3 |= (ADC_SQR3_SQ1_0 | ADC_SQR3_SQ1_2) ;		// init the firt conversion
-	
-	ADC2->CR2 |= ADC_CR2_CONT ;			// continous conversion
-	ADC2->CR2 |= ADC_CR2_ADON ;			// wake up the adc
-	
-	delay_us(1);
-	
-	ADC2->CR2 |= ADC_CR2_RSTCAL;	// reset calib
-    while (ADC2->CR2 & ADC_CR2_RSTCAL);
-	
-	ADC2->CR2 |= ADC_CR2_CAL ;		//calibr  
-	while (ADC2->CR2 & ADC_CR2_CAL);
-	
-	ADC2->CR2 |= ADC_CR2_ADON ;			// again -> start
-	delay_us(1);
-	
 	
 	
 	
 	
 	while (1)
 	{
-     
-		if (check > 2000)
-		{
-			GPIO_Write(PORTC, 13, 1);
-			delay_ms(1000);
-			GPIO_Write(PORTC, 13, 0);
-			delay_ms(1000);
-		}
-		else
-		{
-			GPIO_Write(PORTC, 13, 0);
-			delay_ms(10);
-			GPIO_Write(PORTC, 13, 1);
-			delay_ms(100);
-		}
+		
+		
+		
+
 	}
 
 
@@ -98,20 +45,4 @@ int main(void)
 
 
 
-
-
-
-
-
-
-
-
-	//---------------------------------------
-//	GPIO_Type button;
-//	button.port	= GPIOB;
-//	button.pin	= 0;
-//	button.mode	= INPUT_MODE;
-//	button.mode_type = INPUT_PU_PD;
-//	button.pull = PULL_DOWN;
-//	gpio_init(button);
 
